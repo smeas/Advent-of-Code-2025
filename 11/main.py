ae9dd1a3acf0d2bbb598228428bc77d44038c37a
Jonatan Johansson <smeas@users.noreply.github.com>
2025-12-14 16:35:05 +0100

    day 11 (part 2)

--- 11/main.py
+++ 11/main.py
@@ -1,20 +1,33 @@
 #
 # Day 11
 # Solution 1: 688
-# Solution 2:
+# Solution 2: 293263494406608
 #
 
-import sys
-from dataclasses import dataclass
+from functools import cache
 from typing import Self
 
-raw = open(sys.argv[1]).read()
+raw = open("input.txt").read()
 
 
-@dataclass
 class Node:
     name: str
-    edges: list[Self]
+    down: list[Self]
+
+    def __init__(self, name):
+        self.name = name
+        self.down = []
+
+    def __repr__(self):
+        def name_key(x):
+            return x.name
+        return f"{self.name} -> ({", ".join(map(name_key, self.down))})"
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, value):
+        return self.name == value.name
 
 
 def solution_1():
@@ -34,7 +47,7 @@ def solution_1():
     start = None
     end = None
     for name in names:
-        node = Node(name, [])
+        node = Node(name)
         nodes[name] = node
         if name == "you":
             start = node
@@ -45,7 +58,7 @@ def solution_1():
     for row in rows:
         name, outputs = row
         for output_name in outputs:
-            nodes[name].edges.append(nodes[output_name])
+            nodes[name].down.append(nodes[output_name])
 
     # depth first search
     stack = [start]
@@ -55,9 +68,59 @@ def solution_1():
         if node == end:
             total += 1
         else:
-            stack.extend(node.edges)
+            stack.extend(node.down)
 
     print(total)
 
 
-solution_1()
+def general_solution():
+    names = set()
+    rows = []
+    for line in raw.splitlines():
+        key, right = line.split(": ")
+        outputs = right.split(" ")
+        rows.append((key, outputs))
+        names.add(key)
+        names.update(outputs)
+
+    # create all nodes first
+    nodes = {}  # type:dict[str, Node]
+    for name in names:
+        node = Node(name)
+        nodes[name] = node
+
+    # build graph
+    for row in rows:
+        name, outputs = row
+        for output_name in outputs:
+            nodes[name].down.append(nodes[output_name])
+
+    @cache  # memoize
+    def path_count(start, end):
+        if start == end:
+            return 1
+        return sum(path_count(other, end) for other in start.down)
+
+    you = nodes["you"]
+    svr = nodes["svr"]
+    end = nodes["out"]
+    dac = nodes["dac"]
+    fft = nodes["fft"]
+
+    print(path_count(you, end))  # solution 1
+
+    # solution 2
+    #
+    # count paths from S to E passing through both A and B
+    #
+    # solution can be broken down into parts:
+    #     count(S, A) * count(A, B) * count(B, E) +
+    #     count(S, B) * count(B, A) * count(A, E)
+    print(path_count(svr, fft) * path_count(fft, dac) * path_count(dac, end) +
+          path_count(svr, dac) * path_count(dac, fft) * path_count(fft, end))
+
+    # print(path_count.cache_info())
+
+
+# solution_1()
+general_solution()
